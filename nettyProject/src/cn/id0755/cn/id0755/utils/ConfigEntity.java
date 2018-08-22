e3147a30e33ccbf3bd315c0e04271a4517159108
attosoft <420098712@qq.com>
2018-08-22 17:37:45 +0800

    nettyDemo
    nett服务端和UDP  client通讯

---
+++ nettyProject/src/cn/id0755/cn/id0755/utils/ConfigEntity.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2016 即时通讯网(52im.net) The MobileIMSDK Project. 
+ * All rights reserved.
+ * Project URL:https://github.com/JackJiang2011/MobileIMSDK
+ *  
+ * 即时通讯网(52im.net) - 即时通讯技术社区! PROPRIETARY/CONFIDENTIAL.
+ * Use is subject to license terms.
+ * 
+ * ConfigEntity.java at 2016-2-20 11:25:57, code by Jack Jiang.
+ * You can contact author with jack.jiang@52im.net or jb2011@163.com.
+ */
+package cn.id0755.cn.id0755.utils;
+
+public class ConfigEntity
+{
+	public static String serverIP = "127.0.0.1";
+	public static int serverUDPPort = 9999;
+	public static int localUDPPort = 6685;
+}
