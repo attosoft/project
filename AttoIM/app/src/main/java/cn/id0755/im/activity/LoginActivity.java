8bf483235b988aaf0ac92fd4bf251e316fa2fcf9
attosoft <420098712@qq.com>
2018-08-29 17:41:52 +0800

    登录设置

--- AttoIM/app/src/main/java/cn/id0755/im/activity/LoginActivity.java
+++ AttoIM/app/src/main/java/cn/id0755/im/activity/LoginActivity.java
@@ -3,8 +3,17 @@ package cn.id0755.im.activity;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.os.Handler;
+import android.provider.Settings;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -21,21 +30,31 @@ import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.telephony.PhoneNumberUtils;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.WindowManager;
 import android.view.inputmethod.EditorInfo;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+
+import net.openmob.mobileimsdk.android.conf.ConfigEntity;
+import net.openmob.mobileimsdk.android.core.LocalUDPDataSender;
+import net.openmob.mobileimsdk.android.core.LocalUDPSocketProvider;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Observable;
+import java.util.Observer;
 
 import cn.id0755.im.R;
+import cn.id0755.im.event.IMClientManager;
 import cn.id0755.im.utils.PhoneUtils;
 
 import static android.Manifest.permission.READ_CONTACTS;
@@ -45,6 +64,7 @@ import static android.Manifest.permission.READ_CONTACTS;
  */
 public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<Cursor> {
 
+    private final static String TAG = "LoginActivity";
     /**
      * Id to identity READ_CONTACTS permission request.
      */
@@ -67,6 +87,16 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
     private EditText mPasswordView;
     private View mProgressView;
     private View mLoginFormView;
+
+    /**
+     * 登陆进度提示
+     */
+    private OnLoginProgress onLoginProgress = null;
+    /**
+     * 收到服务端的登陆完成反馈时要通知的观察者（因登陆是异步实现，本观察者将由
+     * ChatBaseEvent 事件的处理者在收到服务端的登陆反馈后通知之）
+     */
+    private Observer onLoginSucessObserver = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -105,6 +135,281 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 //        }
         mPhoneView.setText("13510773022");
         mPasswordView.setText("123456");
+
+
+        // 确保MobileIMSDK被初始化哦（整个APP生生命周期中只需调用一次哦）
+        // 提示：在不退出APP的情况下退出登陆后再重新登陆时，请确保调用本方法一次，不然会报code=203错误哦！
+        IMClientManager.getInstance(this).initMobileIMSDK();
+
+        // 登陆有关的初始化工作
+        initForLogin();
+    }
+
+    private void initForLogin() {
+        // 实例化登陆进度提示封装类
+        onLoginProgress = new OnLoginProgress(this);
+        // 准备好异步登陆结果回调观察者（将在登陆方法中使用）
+        onLoginSucessObserver = new Observer() {
+            @Override
+            public void update(Observable observable, Object data) {
+                // * 已收到服务端登陆反馈则当然应立即取消显示登陆进度条
+                onLoginProgress.showProgressing(false);
+                // 服务端返回的登陆结果值
+                int code = (Integer) data;
+                // 登陆成功
+                if (code == 0) {
+                    //** 提示：登陆/连接 MobileIMSDK服务器成功后的事情在此实现即可
+
+                    // 进入主界面
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    // 同时关闭登陆界面
+                    finish();
+                }
+                // 登陆失败
+                else {
+                    new AlertDialog.Builder(LoginActivity.this)
+                            .setTitle("友情提示")
+                            .setMessage("Sorry，IM服务器连接失败，错误码=" + code)
+                            .setPositiveButton("知道了", null)
+                            .show();
+                }
+            }
+        };
+    }
+
+    /**
+     * 真正的登陆信息发送实现方法。
+     */
+    private void doLoginImpl() {
+        // * 立即显示登陆处理进度提示（并将同时启动超时检查线程）
+        onLoginProgress.showProgressing(true);
+        // * 设置好服务端反馈的登陆结果观察者（当客户端收到服务端反馈过来的登陆消息时将被通知）
+        IMClientManager.getInstance(this).getBaseEventListener()
+                .setLoginOkForLaunchObserver(onLoginSucessObserver);
+
+        // 异步提交登陆id和token
+        new LocalUDPDataSender.SendLoginDataAsync(
+                LoginActivity.this
+                , mPhoneView.getText().toString().trim()
+                , mPasswordView.getText().toString().trim()) {
+            /**
+             * 登陆信息发送完成后将调用本方法（注意：此处仅是登陆信息发送完成
+             * ，真正的登陆结果要在异步回调中处理哦）。
+             *
+             * @param code 数据发送返回码，0 表示数据成功发出，否则是错误码
+             */
+            @Override
+            protected void fireAfterSendLogin(int code) {
+                if (code == 0) {
+                    //
+                    Toast.makeText(getApplicationContext(), "数据发送成功！", Toast.LENGTH_SHORT).show();
+                    Log.d(MainActivity.class.getSimpleName(), "登陆/连接信息已成功发出！");
+                } else {
+                    Toast.makeText(getApplicationContext(), "数据发送失败。错误码是：" + code + "！", Toast.LENGTH_SHORT).show();
+
+                    // * 登陆信息没有成功发出时当然无条件取消显示登陆进度条
+                    onLoginProgress.showProgressing(false);
+                }
+            }
+        }.execute();
+    }
+
+    /**
+     * 获取APP版本信息.
+     */
+    private String getProgrammVersion() {
+        PackageInfo info;
+        try {
+            info = getPackageManager().getPackageInfo(getPackageName(), 0);
+//			versionCode = info.versionCode;
+//			versionName = info.versionName;
+            return info.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+            Log.w(TAG, "读程序版本信息时出错," + e.getMessage(), e);
+            return "N/A";
+        }
+    }
+
+    private boolean CheckNetworkState() {
+        boolean flag = false;
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(
+                Context.CONNECTIVITY_SERVICE);
+        if (manager.getActiveNetworkInfo() != null) {
+            flag = manager.getActiveNetworkInfo().isAvailable();
+        }
+        if (!flag) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setIcon(android.R.drawable.ic_dialog_alert);
+            builder.setTitle("Network not avaliable");//
+            builder.setMessage("Current network is not avaliable, set it?");//
+            builder.setPositiveButton("Setting", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS)); //直接进入手机中的wifi网络设置界面
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.cancel();
+                }
+            });
+            builder.create();
+            builder.show();
+        }
+
+        return flag;
+    }
+
+    /**
+     * 登陆处理。
+     *
+     * @see #doLoginImpl()
+     */
+    private void doLogin() {
+        if (!CheckNetworkState())
+            return;
+
+        // 设置服务器地址和端口号
+        String serverIP = "172.20.205.60";
+        String serverPort = "7901";
+        if (!(serverIP.trim().length() <= 0)
+                && !(serverPort.trim().length() <= 0)) {
+            // 无条件重置socket，防止首次登陆时用了错误的ip或域名，下次登陆时sendData中仍然使用老的ip
+            // 说明：本行代码建议仅用于Demo时，生产环境下是没有意义的，因为你的APP里不可能连IP都搞错了
+            LocalUDPSocketProvider.getInstance().closeLocalUDPSocket();
+
+            ConfigEntity.serverIP = serverIP.trim();
+            try {
+                ConfigEntity.serverUDPPort = Integer.parseInt(serverPort.trim());
+            } catch (Exception e2) {
+                Toast.makeText(getApplicationContext(), "请输入合法的端口号！", Toast.LENGTH_SHORT).show();
+                return;
+            }
+        } else {
+            Toast.makeText(getApplicationContext(), "请确保服务端地址和端口号都不为空！", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        // 发送登陆数据包
+        if (mPhoneView.getText().toString().trim().length() > 0) {
+            doLoginImpl();
+        } else
+            Log.e(MainActivity.class.getSimpleName()
+                    , "txt.len=" + (mPhoneView.getText().toString().trim().length()));
+    }
+
+    /**
+     * 登陆进度提示和超时检测封装实现类.
+     */
+    private class OnLoginProgress {
+        /**
+         * 登陆的超时时间定义
+         */
+        private final static int RETRY_DELAY = 6000;
+
+        private Handler handler = null;
+        private Runnable runnable = null;
+        // 重试时要通知的观察者
+        private Observer retryObsrver = null;
+
+        private ProgressDialog progressDialogForPairing = null;
+        private Activity parentActivity = null;
+
+        public OnLoginProgress(Activity parentActivity) {
+            this.parentActivity = parentActivity;
+            init();
+        }
+
+        private void init() {
+            progressDialogForPairing = new ProgressDialog(parentActivity);
+            progressDialogForPairing.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            progressDialogForPairing.setTitle("登陆中");
+            progressDialogForPairing.setMessage("正在登陆中，请稍候。。。");
+            progressDialogForPairing.setCanceledOnTouchOutside(false);
+
+            handler = new Handler();
+            runnable = new Runnable() {
+                @Override
+                public void run() {
+                    onTimeout();
+                }
+            };
+        }
+
+        /**
+         * 登陆超时后要调用的方法。
+         */
+        private void onTimeout() {
+            // 本观察者中由用户选择是否重试登陆或者取消登陆重试
+            new AlertDialog.Builder(LoginActivity.this)
+                    .setTitle("超时了")
+                    .setMessage("登陆超时，可能是网络故障或服务器无法连接，是否重试？")
+                    .setPositiveButton("重试！", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // 确认要重试时（再次尝试登陆哦）
+                            doLogin();
+                        }
+                    })
+                    .setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // 不需要重试则要停止“登陆中”的进度提示哦
+                            OnLoginProgress.this.showProgressing(false);
+                        }
+                    })
+                    .show();
+        }
+
+        /**
+         * 显示进度提示.
+         *
+         * @param show
+         */
+        public void showProgressing(boolean show) {
+            // 显示进度提示的同时即启动超时提醒线程
+            if (show) {
+                showLoginProgressGUI(true);
+
+                // 先无论如何保证利重试检测线程在启动前肯定是处于停止状态
+                handler.removeCallbacks(runnable);
+                // 启动
+                handler.postDelayed(runnable, RETRY_DELAY);
+            }
+            // 关闭进度提示
+            else {
+                // 无条件停掉延迟重试任务
+                handler.removeCallbacks(runnable);
+
+                showLoginProgressGUI(false);
+            }
+        }
+
+        /**
+         * 进度提示时要显示或取消显示的GUI内容。
+         *
+         * @param show true表示显示gui内容，否则表示结速gui内容显示
+         */
+        private void showLoginProgressGUI(boolean show) {
+            // 显示登陆提示信息
+            if (show) {
+                try {
+                    if (parentActivity != null && !parentActivity.isFinishing())
+                        progressDialogForPairing.show();
+                } catch (WindowManager.BadTokenException e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+            }
+            // 关闭登陆提示信息
+            else {
+                // 此if语句是为了保证延迟线程里不会因Activity已被关闭而此处却要非法地执行show的情况（此判断可趁为安全的show方法哦！）
+                if (parentActivity != null && !parentActivity.isFinishing())
+                    progressDialogForPairing.dismiss();
+            }
+        }
     }
 
     private void populateAutoComplete() {
@@ -198,8 +503,9 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
             showProgress(true);
-            mAuthTask = new UserLoginTask(phone, password);
-            mAuthTask.execute((Void) null);
+//            mAuthTask = new UserLoginTask(phone, password);
+//            mAuthTask.execute((Void) null);
+            doLogin();
         }
     }
 
@@ -319,24 +625,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
 
         @Override
         protected Boolean doInBackground(Void... params) {
-            // TODO: attempt authentication against a network service.
-
-            try {
-                // Simulate network access.
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                return false;
-            }
-
-//            for (String credential : DUMMY_CREDENTIALS) {
-//                String[] pieces = credential.split(":");
-//                if (pieces[0].equals(mEmail)) {
-//                    // Account exists, return true if the password matches.
-//                    return pieces[1].equals(mPassword);
-//                }
-//            }
-
-            // TODO: register the new account here.
+//            doLogin();
             return true;
         }
 
